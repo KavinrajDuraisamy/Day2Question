073388c1744ab3ca936c43691a05d2015e408b57
Kavin Durai <Kavinraj.Duraisamy@lntinfotech.com>
2020-12-16 17:14:32 +0530

    testcode

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("a value = " + a);
+            Console.WriteLine("b value = " + b);
+            Console.WriteLine("a+b= " + c);
+            Console.WriteLine( a+" "+b);
+            Console.WriteLine($"a={a} \nb={b} \na+b={c}");
+            */
+
+            int a = int.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            char c = Convert.ToChar(Console.ReadLine());
+            string d = Console.ReadLine();
+            Console.WriteLine($"a={a} \nb={b} \nc={c} \nd={d}");
+
+        }
+    }
+}
